14a3dca7c064f77e2917a172ebc2311234cb6e96
Carl Tanner <rocketeer.captproton@gmail.com>
2017-02-04 20:00:36 -0800

    ADDED: current_user can CRUD contacts at /contacts

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
   factory :contact do
-    first_name "MyString"
-    last_name "MyString"
-    email_address "MyText"
-    street_one "MyText"
-    street_two "MyText"
-    city "MyString"
-    state "MyString"
-    zip "MyString"
-    primary_phone "MyString"
-    secondary_phone "MyString"
-    addl_phone_one "MyString"
-    addl_phone_two "MyString"
-    lead_source "MyString"
+    first_name "Firstname"
+    last_name "LastName"
+    email_address "email@email.com"
+    street_one "1 street"
+    street_two "Apt 2"
+    city "City"
+    state "UT"
+    zip "94044"
+    primary_phone "455-555-1212"
+    secondary_phone "455-555-1213"
+    addl_phone_one "455-555-1214"
+    addl_phone_two "455-555-1217"
+    lead_source "455-555-1219"
   end
   factory :user do
     
