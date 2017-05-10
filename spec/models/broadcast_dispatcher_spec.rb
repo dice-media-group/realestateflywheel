e649cd58ab60b23ff4a323c4ef4adb7d84ce8dc4
Carl Tanner <rocketeer.captproton@gmail.com>
2017-05-10 07:40:23 -0700

    ADDED:  model and feature specs

--- spec/models/broadcast_dispatcher_spec.rb
+++ spec/models/broadcast_dispatcher_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe BroadcastDispatcher, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   let(:password)      { "123456789" }
   let(:current_user)  { create( :user, password: password, password_confirmation: password ) }
-  let(:broadcast)     { create(:broadcast, user_id: current_user.id) }
+  let(:broadcast)     { create(:broadcast) }
   # let(:contacts)     { create(:contact, user_id: current_user.id) }
   
   
   describe '#send' do
     it 'works' do
-      # expect(BroadcastDispatcher.new(broadcast: broadcast).send).to eq(["mono", "tag"])
+      # expect(BroadcastDispatcher.new(broadcast: broadcast).send).to eq("someting")
     end
   end
   
   describe '#_collect_contacts_to_receive_broadcast' do
-    let(:contact)     { create(:contact, user_id: current_user.id) }
-    let(:contacts)    { build_stubbed_list(:contact, 25) }
+    let(:contact)     { create(:user_with_contacts, contacts_count: 1).contacts.first }
+    let(:contacts)    { create(:user_with_contacts).contacts }
     
     it 'has a valid contact factory' do
      expect(contact).to be_valid
@@ -26,21 +26,16 @@ RSpec.describe BroadcastDispatcher, type: :model do
      expect(contact.tag_list).to eq(["mono", "tag1"])
     end
 
-    it 'has broadcast tags eq to ["mono", "tag"]' do
-     expect(broadcast.tag_list).to eq(["mono", "tag"])
-    end
-    
     it 'returns a list of tagged contacts' do
       contact_list = BroadcastDispatcher.new(broadcast: broadcast)
         ._collect_contacts_to_receive_broadcast(roster_tags: Array(broadcast.tag_list))
       expect(contact_list).not_to be_empty
-      
     end
     
     it 'returns an Array of contacts' do
       contact_list = BroadcastDispatcher.new(broadcast: broadcast)
         ._collect_contacts_to_receive_broadcast(roster_tags: Array(broadcast.tag_list))
-      expect(contact_list).to be_kind_of(Array)
+      expect(contact_list).to match_array(contacts)
             
     end
     
